e2afeef1ca91091621ca723f54bb17061360c269
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2014-07-10 01:47:12 -0700

    Fix setup.py to use setuptools if available.

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='gcalcli',
       version='3.1',
