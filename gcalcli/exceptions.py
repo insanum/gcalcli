8f80a874534170670870a4aa60be030082e181b0
Joshua Crowgey <jcrowgey@uw.edu>
2019-07-10 14:41:23 -0700

    Make all_cals count errors more explicit
    When something goes wrong with parsing your --calendar option (or your
    defaults are wrong, or whatever) and you end up with 0 calendars
    available, in the context where the calendar count has to be == 1, we
    say "You must select only one calendar".  But this is confusing because
    the problem is not that you have selected > 1, but that you ended up
    with 0.  Hopefully this messaging change helps people realize what went
    wrong faster.

--- gcalcli/exceptions.py
+++ gcalcli/exceptions.py
@@ -6,3 +6,9 @@ class ValidationError(Exception):
     def __init__(self, message):
         super(ValidationError, self).__init__(message)
         self.message = message
+
+def raise_one_cal_error(cals):
+    raise GcalcliError(
+        'You must only specify a single calendar\n'
+        'Calendars: {}\n'.format(cals)
+    )
