c62e70788b9d081151233fd42ace096ab09b3d5f
Morteza Nourelahi Alamdari <me@mortezana.com>
2019-07-28 01:33:14 +0200

    Flake8 fixes #430

--- gcalcli/gcal.py
+++ gcalcli/gcal.py
@@ -664,7 +664,7 @@ class GoogleCalendarInterface:
                 event['s'].strftime('%I:%M').lstrip('0').rjust(5) + \
                 event['s'].strftime('%p').lower()
             if self.options.get('show_end_time'):
-                tmp_end_time_str =  event['e'].strftime('%I:%M').lstrip('0').ljust(5) + \
+                tmp_end_time_str = event['e'].strftime('%I:%M').lstrip('0') + \
                     event['e'].strftime('%p').lower()
                 time_fmt = '  ' + time_format + '-' + time_format + '  %s\n'
             else:
@@ -697,7 +697,8 @@ class GoogleCalendarInterface:
         else:
             fmt = '  ' + time_format + '  %s\n'
             self.printer.msg(
-                    time_fmt % (tmp_time_str, tmp_end_time_str, self._valid_title(event).strip()),
+                    time_fmt % (tmp_time_str, tmp_end_time_str,
+                                self._valid_title(event).strip()),
                     event_color
             )
 
