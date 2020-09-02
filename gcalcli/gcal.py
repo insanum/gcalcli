9498c6c64e93b46ef71947feb24a960534d862a7
Shai Coleman <shai.coleman@cashanalytics.com>
2020-09-02 21:49:29 +0100

    Fix "KeyError: 'creator'" bug with private events

--- gcalcli/gcal.py
+++ gcalcli/gcal.py
@@ -633,7 +633,7 @@ class GoogleCalendarInterface:
 
             if self.details.get('email'):
                 output += '\t%s' % (event['creator']['email'].strip()
-                                    if 'email' in event['creator'] else '')
+                                    if 'creator' in event and 'email' in event['creator'] else '')
 
             output = '%s\n' % output.replace('\n', '''\\n''')
             sys.stdout.write(output)
