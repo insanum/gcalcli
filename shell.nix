b9c4e7be0b996458844e8151c4724fae16b1cd0b
Gustavo Porto <gustavo@portosanti.com>
2022-08-31 13:27:45 +0100

    chore: update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 { pkgs ? import <nixpkgs> { } }:
 
 let
-  python39WithPackages =
-    (pkgs.python39.withPackages (pythonPackages: with pythonPackages; [ tox ]));
-in pkgs.mkShell { buildInputs = [ python39WithPackages ]; }
+  python38WithPackages =
+    (pkgs.python38.withPackages (pythonPackages: with pythonPackages; [ tox ]));
+in pkgs.mkShell { buildInputs = [ python38WithPackages ]; }
