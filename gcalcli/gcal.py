7095403ead952b531609f56f6bcd9be94d63a617
Michael Hoffman <michaelmhoffman@users.noreply.github.com>
2023-10-25 12:20:29 -0400

    style: remove tricky method defaults
    Quiet flake8-bugbear B006 and B008 warnings.

--- gcalcli/gcal.py
+++ gcalcli/gcal.py
@@ -43,6 +43,7 @@ from collections import namedtuple
 EventTitle = namedtuple('EventTitle', ['title', 'color'])
 
 CONFERENCE_DATA_VERSION = 1
+PRINTER = Printer()
 
 
 class GoogleCalendarInterface:
@@ -63,7 +64,7 @@ class GoogleCalendarInterface:
 
     UNIWIDTH = {'W': 2, 'F': 2, 'N': 1, 'Na': 1, 'H': 1, 'A': 1}
 
-    def __init__(self, cal_names=[], printer=Printer(), **options):
+    def __init__(self, cal_names=(), printer=PRINTER, **options):
         self.cals = []
         self.printer = printer
         self.options = options
